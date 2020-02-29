7b76da8c4348cdb90f3c1840be345542016cbc7d
Eyal <eyal.shaar@gmail.com>
2020-02-29 13:34:08 +0200

    Update

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -9,35 +9,35 @@ class Directory extends Component {
 		this.state = {
 			sections: [
 				{
+					id: 1,
 					title: 'hats',
 					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-					id: 1,
 					linkUrl: 'shop/hats'
 				},
 				{
+					id: 2,
 					title: 'jackets',
 					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-					id: 2,
 					linkUrl: 'shop/jackets'
 				},
 				{
+					id: 3,
 					title: 'sneakers',
 					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-					id: 3,
 					linkUrl: 'shop/sneakers'
 				},
 				{
+					id: 4,
 					title: 'womens',
 					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
 					size: 'large',
-					id: 4,
 					linkUrl: 'shop/womens'
 				},
 				{
+					id: 5,
 					title: 'mens',
 					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
 					size: 'large',
-					id: 5,
 					linkUrl: 'shop/mens'
 				}
 			]
@@ -48,8 +48,8 @@ class Directory extends Component {
 		return (
 			<div className="directory-menu">
 				{
-					this.state.sections.map(({ title, id }) => 
-						<MenuItem key={id} title={title} />
+					this.state.sections.map(({ id, title, imageUrl, size }) => 
+						<MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
 					)
 				}
 			</div>
