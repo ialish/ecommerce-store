6cfba0970e063febc613e01cbb442b6345f00317
Eyal <eyal.shaar@gmail.com>
2020-03-02 11:30:07 +0200

    Update

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,46 +1,14 @@
 import React, { Component } from 'react';
-import './Directory.scss';
 
+import './Directory.scss';
+import SECTIONS_DATA from './sections-data'
 import MenuItem from '../MenuItem/MenuItem';
 
 class Directory extends Component {
 	constructor() {
 		super();
 		this.state = {
-			sections: [
-				{
-					id: 1,
-					title: 'hats',
-					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-					linkUrl: 'shop/hats'
-				},
-				{
-					id: 2,
-					title: 'jackets',
-					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-					linkUrl: 'shop/jackets'
-				},
-				{
-					id: 3,
-					title: 'sneakers',
-					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-					linkUrl: 'shop/sneakers'
-				},
-				{
-					id: 4,
-					title: 'women',
-					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-					linkUrl: 'shop/women',
-					size: 'large'
-				},
-				{
-					id: 5,
-					title: 'men',
-					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-					linkUrl: 'shop/men',
-					size: 'large'
-				}
-			]
+			sections: SECTIONS_DATA
 		};
 	}
 	
---
+++ src/components/Directory/sections-data.js
@@ -0,0 +1,36 @@
+const SECTIONS_DATA = [
+	{
+		id: 1,
+		title: 'hats',
+		imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+		linkUrl: 'shop/hats'
+	},
+	{
+		id: 2,
+		title: 'jackets',
+		imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+		linkUrl: 'shop/jackets'
+	},
+	{
+		id: 3,
+		title: 'sneakers',
+		imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+		linkUrl: 'shop/sneakers'
+	},
+	{
+		id: 4,
+		title: 'women',
+		imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+		linkUrl: 'shop/women',
+		size: 'large'
+	},
+	{
+		id: 5,
+		title: 'men',
+		imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+		linkUrl: 'shop/men',
+		size: 'large'
+	}
+]
+
+export default SECTIONS_DATA;
